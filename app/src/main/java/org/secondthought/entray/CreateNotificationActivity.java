38a868e4bc1f33235d5251189b792686bdd1ae94
Andrew Hogue <ahogue@foursquare.com>
2014-12-21 14:28:08 -0500

    Remove menu

--- app/src/main/java/org/secondthought/entray/CreateNotificationActivity.java
+++ app/src/main/java/org/secondthought/entray/CreateNotificationActivity.java
@@ -19,14 +19,6 @@ public class CreateNotificationActivity extends ActionBarActivity {
         setContentView(R.layout.activity_create_notification);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_create_notification, menu);
-        return true;
-    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
