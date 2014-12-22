f7dba3a4c4c77e3009966edb79f7b8fb264d54b9
Andrew Hogue <ahogue@foursquare.com>
2014-12-22 15:50:02 -0500

    Handle voice input

--- app/src/main/java/org/secondthought/entray/CreateNotificationActivity.java
+++ app/src/main/java/org/secondthought/entray/CreateNotificationActivity.java
@@ -1,24 +1,29 @@
 package org.secondthought.entray;
 
-import android.app.Activity;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
-public class CreateNotificationActivity extends ActionBarActivity {
+public class CreateNotificationActivity extends ActionBarActivity implements View.OnClickListener {
+    protected static final int REQUEST_OK = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_notification);
+
+        findViewById(R.id.voice_button).setOnClickListener(this);
 
         Intent intent = getIntent();
         String action = intent.getAction();
@@ -69,5 +74,25 @@ public class CreateNotificationActivity extends ActionBarActivity {
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         // mId allows you to update the notification later on.
         mNotificationManager.notify(0, mBuilder.build());
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
+        try {
+            startActivityForResult(intent, REQUEST_OK);
+        } catch (Exception e) {
+            Toast.makeText(this, "Error initializing speech to text engine.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_OK && resultCode == RESULT_OK) {
+            ArrayList<String> utterances = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            createNotification(utterances.get(0));
+        }
     }
 }
